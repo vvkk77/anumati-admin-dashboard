60cca1875207da0b63f677303b0d0f10ba45f325
Vishwajeet Kushmariya <k.vish.7777@gmail.com>
2020-03-27 05:06:30 +0530

    Updates

---
+++ precache-manifest.5afe4f4260741cb4f688de654eb4bb65.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89613fb2db3eb06ba49862f44f9f5fe",
+    "url": "/anumati-admin-dashboard/index.html"
+  },
+  {
+    "revision": "ba28cb256c007139cea2",
+    "url": "/anumati-admin-dashboard/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "66df91c51c3aeaeb917e",
+    "url": "/anumati-admin-dashboard/static/css/main.c97060e4.chunk.css"
+  },
+  {
+    "revision": "ba28cb256c007139cea2",
+    "url": "/anumati-admin-dashboard/static/js/2.b270fc87.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/anumati-admin-dashboard/static/js/2.b270fc87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66df91c51c3aeaeb917e",
+    "url": "/anumati-admin-dashboard/static/js/main.9c384806.chunk.js"
+  },
+  {
+    "revision": "3e197426c1600da7cf7d",
+    "url": "/anumati-admin-dashboard/static/js/runtime-main.83706527.js"
+  },
+  {
+    "revision": "72e1b5bfb30059c71b4d4df56ac2b199",
+    "url": "/anumati-admin-dashboard/static/media/anumatiLogo.72e1b5bf.jpg"
+  },
+  {
+    "revision": "03f1bbc50ff428ea33db12d5d3f5f8d9",
+    "url": "/anumati-admin-dashboard/static/media/vertical-dots.03f1bbc5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/anumati-police-dashboard/precache-manifest.6eb3bce70d45a82697214b8b1d61e4dd.js"
+  "/anumati-admin-dashboard/precache-manifest.5afe4f4260741cb4f688de654eb4bb65.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/anumati-police-dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/anumati-admin-dashboard/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
