1b3eb1730f2a6342b3167bc2ba9cc2cf08c45cdd
Vishwajeet Kushmariya <k.vish.7777@gmail.com>
2020-03-27 04:04:08 +0530

    Updates

---
+++ precache-manifest.160259983b45101d353a74ff5a5e03aa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fbf2e9861e4289c2a4e8dee51374236",
+    "url": "/anumati-police-dashboard/index.html"
+  },
+  {
+    "revision": "400853f93a6cd3435410",
+    "url": "/anumati-police-dashboard/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "3cea23d2b9fc4399c798",
+    "url": "/anumati-police-dashboard/static/css/main.c97060e4.chunk.css"
+  },
+  {
+    "revision": "400853f93a6cd3435410",
+    "url": "/anumati-police-dashboard/static/js/2.58706d9e.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/anumati-police-dashboard/static/js/2.58706d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cea23d2b9fc4399c798",
+    "url": "/anumati-police-dashboard/static/js/main.4e187939.chunk.js"
+  },
+  {
+    "revision": "5cd1111115401f87086f",
+    "url": "/anumati-police-dashboard/static/js/runtime-main.18c80846.js"
+  },
+  {
+    "revision": "72e1b5bfb30059c71b4d4df56ac2b199",
+    "url": "/anumati-police-dashboard/static/media/anumatiLogo.72e1b5bf.jpg"
+  },
+  {
+    "revision": "03f1bbc50ff428ea33db12d5d3f5f8d9",
+    "url": "/anumati-police-dashboard/static/media/vertical-dots.03f1bbc5.svg"
+  }
+]);
